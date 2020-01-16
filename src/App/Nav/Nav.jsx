8b76860727169b2e3212a87b06e9a37df4935e7f
Jennifer Han <jenniferhan@Jennifers-MacBook-Pro.local>
2020-01-15 21:36:26 -0800

    Hamburger block update

---
+++ src/App/Nav/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Nav.scss';
+
+const Nav = () => {
+    return (
+        <nav className='Nav'>
+
+            <div className="hamburger">Hamburger</div>
+
+            <div className="links">
+                <a href="#">Welcome</a>
+                <a href="#">Services</a>
+                <a href="#">Contact</a>
+            </div>
+
+        </nav>
+    )
+};
+
+export default Nav;
