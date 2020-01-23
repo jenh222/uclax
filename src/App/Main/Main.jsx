09f6fc8c75415a237825446c51498d5c8373b23e
Jennifer Han <jenniferhan@Jennifers-MacBook-Pro.local>
2020-01-22 21:38:56 -0800

    updated hamburger toggle

--- src/App/Main/Main.jsx
+++ src/App/Main/Main.jsx
@@ -3,7 +3,15 @@ import './Main.scss';
 
 const Main = () => {
     return (
-        <main className='Main'>Component Main</main>
+        <main className='Main'>
+            
+            <div className="columns">
+                <div className="col col1">Col1</div>
+                <div className="col col2">Col2</div>
+                <div className="col col3">Col3</div>
+                <div className="col col4">Col4</div>
+            </div>
+        </main>
     )
 };
 
--- src/App/Nav/Nav.jsx
+++ src/App/Nav/Nav.jsx
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Nav.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 const Nav = () => {
+
+    const [showMenu, updateShowMenu] = useState(true);
+    
+    const handleHamburgerClick = () => {
+            console.log('you got the hamburger');
+
+            let timbuktu;
+
+            if (showMenu === true) {
+                timbuktu = false;
+            } else {
+                timbuktu = true;
+            }
+
+            updateShowMenu(timbuktu);
+    }
+
+
     return (
         <nav className='Nav'>
 
-            <div className="hamburger">Hamburger</div>
+            <div className="hamburger" onClick={ handleHamburgerClick }>
+                <FontAwesomeIcon icon={faBars}/>
+            </div>
 
+
+        {
+           showMenu &&
             <div className="links">
                 <a href="#">Welcome</a>
                 <a href="#">Services</a>
                 <a href="#">Contact</a>
             </div>
-
+        }
         </nav>
     )
 };
